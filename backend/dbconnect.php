87cd7b1eef08ee3f0c42affcab8cfab22b1fcf67
I G N Bagus Indra <ignbagusindra.82@gmail.com>
2023-08-11 11:52:25 +0800

    Update dbconnect.php

--- backend/dbconnect.php
+++ backend/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-	 $dbhost = "103.227.176.10";
-	 $dbuser = "radioguntur_user";
-	 $dbpass = "R4d10#$%)(*";
-	 $dbname = "radioguntur_db";
+	 $dbhost = ""; // server
+	 $dbuser = "";
+	 $dbpass = "";
+	 $dbname = "";
 	 
 	 $con = new MySQLi($dbhost,$dbuser,$dbpass,$dbname);
     
